32ebfe06c493b9ce5554b8e816a45246c3812bdf
david.steele <david.i.steele@gmail.com>
2017-01-28 11:35:36 +0000

    Changed vertex format to 3d pos

--- HelloTK/Game.cs
+++ HelloTK/Game.cs
@@ -31,16 +31,16 @@ namespace HelloTK
         {
             Vertex[] verts = new Vertex[3]
             {
-                new Vertex(new Vector2(-1.0f, 0.0f), new Vector2(0,0), Color.Red),
-                new Vertex(new Vector2(-1.0f, -1.0f), new Vector2(0,1), Color.Blue),
-                new Vertex(new Vector2( 0.0f, -1.0f), new Vector2(1,1), Color.Purple),
+                new Vertex(new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(0,0), Color.Red),
+                new Vertex(new Vector3(-1.0f, -1.0f, 0.0f), new Vector2(0,1), Color.Blue),
+                new Vertex(new Vector3( 0.0f, -1.0f, 0.0f), new Vector2(1,1), Color.Purple),
             };
 
             VertexBuffer<Vertex> vertexBuffer = new VertexBuffer<Vertex>(verts, Vertex.SizeInBytes);
             VertexArray<Vertex> vertexArray = new VertexArray<Vertex>(vertexBuffer, shader,
-                new VertexAttribute("aPosition", 2, VertexAttribPointerType.Float, Vertex.SizeInBytes, 0),
-                new VertexAttribute("aTexCoords", 2, VertexAttribPointerType.Float, Vertex.SizeInBytes, 8),
-                new VertexAttribute("aColor", 4, VertexAttribPointerType.Float, Vertex.SizeInBytes, 16)
+                new VertexAttribute("aPosition", 3, VertexAttribPointerType.Float, Vertex.SizeInBytes, 0),
+                new VertexAttribute("aTexCoords", 2, VertexAttribPointerType.Float, Vertex.SizeInBytes, 12),
+                new VertexAttribute("aColor", 4, VertexAttribPointerType.Float, Vertex.SizeInBytes, 20)
             );
             vertexBuffer.AddVertexArray(vertexArray);
             Renderer renderer = new Renderer();
@@ -53,10 +53,10 @@ namespace HelloTK
         {
             Vertex[] quad = new Vertex[4]
             {
-                new Vertex(new Vector2(0.0f, 0.0f), new Vector2(0,0), Color.Green),
-                new Vertex(new Vector2(1.0f, 0.0f), new Vector2(1,1), Color.Orange),
-                new Vertex(new Vector2(0.0f, 1.0f), new Vector2(1,1), Color.White),
-                new Vertex(new Vector2(1.0f, 1.0f), new Vector2(0,1), Color.Yellow),
+                new Vertex(new Vector3(0.0f, 0.0f, 0.0f), new Vector2(0,0), Color.Green),
+                new Vertex(new Vector3(1.0f, 0.0f, 0.0f), new Vector2(1,1), Color.Orange),
+                new Vertex(new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1,1), Color.White),
+                new Vertex(new Vector3(1.0f, 1.0f, 0.0f), new Vector2(0,1), Color.Yellow),
             };
             uint[] indices = new uint[6]
             {
@@ -65,9 +65,9 @@ namespace HelloTK
             VertexBuffer<Vertex> quadVertexBuffer = new VertexBuffer<Vertex>(quad, Vertex.SizeInBytes);
             // TODO Define VertexFormat alongside Vertex, and autogen VertexArray from it.
             VertexArray<Vertex> qvertexArray = new VertexArray<Vertex>(quadVertexBuffer, shader,
-                new VertexAttribute("aPosition", 2, VertexAttribPointerType.Float, Vertex.SizeInBytes, 0),
-                new VertexAttribute("aTexCoords", 2, VertexAttribPointerType.Float, Vertex.SizeInBytes, Vector2.SizeInBytes),
-                new VertexAttribute("aColor", 4, VertexAttribPointerType.Float, Vertex.SizeInBytes, Vector2.SizeInBytes * 2)
+                new VertexAttribute("aPosition", 3, VertexAttribPointerType.Float, Vertex.SizeInBytes, 0),
+                new VertexAttribute("aTexCoords", 2, VertexAttribPointerType.Float, Vertex.SizeInBytes, Vector3.SizeInBytes),
+                new VertexAttribute("aColor", 4, VertexAttribPointerType.Float, Vertex.SizeInBytes, Vector2.SizeInBytes + Vector3.SizeInBytes)
             );
 
             quadVertexBuffer.AddVertexArray(qvertexArray);
--- HelloTK/Vertex.cs
+++ HelloTK/Vertex.cs
@@ -11,23 +11,23 @@ namespace HelloTK
 {
     struct Vertex
     {
-        public Vector2 position;
+        public Vector3 position;
         public Vector2 texCoords;
         public Vector4 color;
-        public static int SizeInBytes { get { return Vector2.SizeInBytes * 2 + Vector4.SizeInBytes; } }
+        public static int SizeInBytes { get { return Vector2.SizeInBytes + Vector3.SizeInBytes + Vector4.SizeInBytes; } }
         public Color Color 
         {
             get { return Color.FromArgb((int)(color.W*255), (int)(color.X*255), (int)(color.Y*255), (int)(color.Z*255) ); }
             set { this.color = new Vector4(value.R/255.0f, value.G/255.0f, value.B/255.0f, value.A/255.0f); }
         }
 
-        public Vertex(Vector2 position, Vector2 texCoords, Vector4 color)
+        public Vertex(Vector3 position, Vector2 texCoords, Vector4 color)
         {
             this.position = position;
             this.texCoords = texCoords;
             this.color = color;
         }
-        public Vertex(Vector2 position, Vector2 texCoords, Color color)
+        public Vertex(Vector3 position, Vector2 texCoords, Color color)
         {
             this.position = position;
             this.texCoords = texCoords;
